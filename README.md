82a4014a82a9b0c7776b86ca48bea85baceb6b88
olino3101 <olivier_nault@hotmail.com>
2025-10-04 20:18:38 +0900

    fuck

--- README.md
+++ README.md
@@ -4,6 +4,10 @@ React Native app to search and view 42 School student profiles using the 42 API.
 
 ## Quick Start
 
+create .env and add these lines
+UID="u-s4t2ud-491a128c812e24b247ccd85a0bc2488669d0ef0936c7a809b89e89826419cb29"
+SECRET="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"
+
 ```bash
 git clone https://github.com/olino3101/swifty-Companion.git
 cd swifty-Companion/Swifty
--- src/utils/GetToken.js
+++ src/utils/GetToken.js
@@ -1,9 +1,15 @@
 import axios from 'axios';
+import Config from "react-native-config";
 
-const UID = 'u-s4t2ud-491a128c812e24b247ccd85a0bc2488669d0ef0936c7a809b89e89826419cb29';
-const SECRET = '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';
+const UID = Config.UID;
+const SECRET = Config.SECRET;
 
 async function getToken() {
+  console.log(UID, SECRET, "sdfsdfsdfsdfsfsdfsdf");
+  if (!UID || !SECRET) {
+    console.error('Missing UID or SECRET in environment variables');
+    return null;
+  }
   const tokenUrl = 'https://api.intra.42.fr/oauth/token';
   const params = new URLSearchParams();
   params.append('grant_type', 'client_credentials');
